71c7ebe73888adf18c61b3a226d5d69dcfbf730d
ShisenSni <96551212+ShisenSni@users.noreply.github.com>
2022-08-28 13:50:29 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+int main() {
+  
+int data = 255;
+double playload = 6.153;
+bool flag = true;
+char ch = '#';
+
+    
+int n = 10;
+int n2 = n + 100;
+int n3 = n2 - 20;
+int n4 = n3 * 3;
+int n5 = n4 / 2;
+
+    
+    float g = 0;
+    float h = 1;
+    double q = 1.5426;
+  
+int value = 3;
+int value2 = value + 1; 
+
+
+  
+  std::cout << "playload value is : " << playload << std::endl;
+  std::cout << "flag value is : " << flag << std::endl;
+  std::cout << "ch value is : " << ch << std::endl;
+  std::cout << n2 << std::endl;
+  std::cout << n3 << std::endl;
+  std::cout << n4<< std::endl;
+  std::cout << n5 << std::endl; 
+  std::cout << value2 << std::endl;
+  
+float userID = 0 * 12;
+userID = 5;
+std::cout << userID << std::endl;
+  return 0;
+  }
+    // 5. Someone wrote a program with errors. 
+    // Uncomment below code and find these errors and tell why did they arise
+    
+ //
+    
